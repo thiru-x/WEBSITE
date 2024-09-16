49dd63fb177e3e480988e13ede9ad3747fcaa434
thiru-x <23csb53@karpagamtech.ac.in>
2024-09-16 20:32:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('orderForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Order submitted successfully!');
+    // Add your form submission code here
+});
